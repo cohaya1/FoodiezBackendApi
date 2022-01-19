0478879e3825b993ef670c721c8f79da9b6ebece
Chika Ohaya <chika.ohaya@gmail.com>
2022-01-18 23:05:39 -0500

    Created FoodiezAPi

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+
+import {ApolloServer} from 'apollo-server';
+import {Query} from './resolvers/Query';
+import {typeDefs} from './schema';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query,
+    }
+  });
+
+server.listen().then(({url}) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
